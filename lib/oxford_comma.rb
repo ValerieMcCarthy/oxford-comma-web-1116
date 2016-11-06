9bde352c3e47126c4954a89d5b98a112dfd5fb3a
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-06 17:25:50 -0500

    Good grammer implemented.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+	if array.length == 1
+		array.join
+	elsif array.length == 2
+		array.join(" and ")
+	else
+		last = array.pop
+		mod_array = array.join(", ")
+		mod_array << ", and #{last}"
+		mod_array
+	end
 end
